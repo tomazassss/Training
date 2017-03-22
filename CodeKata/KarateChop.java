f107b7dff1e6f009b8a1b07c1aafc8dc5f414cd6
Tomas <tomazassss@gmail.com>
2017-03-22 14:40:47 +0200

    Add files via upload

--- CodeKata/KarateChop.java
+++ CodeKata/KarateChop.java
@@ -1,3 +1,9 @@
+package com.viavi.vsamonitor.tests;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -76,8 +82,13 @@ public class KarateChop
 
     /**
      * Recursive implementation of binary chop.
-     * Pros:
-     * Cons:
+     * Pros: Much faster.
+     * Cons: Harder to maintain and understand.
+     *
+     * Was easier to write. Reused a lot of knowledge form previous implementation bot directly and indirectly.
+     * Added few tweaks I did not thought about in the previous implementation.
+     * Still don't feel like it's bugless, easiest to understand or most efficient solution.
+     *
      * @param searchNum
      * @param searchArray
      * @return the integer index of the target in the array, or -1 if the target is not in the array.
@@ -86,9 +97,13 @@ public class KarateChop
     {
         int result = -1;
 
-        if(searchArray.length != 0)
+        if(searchArray.length == 1 && searchArray[0] == searchNum)
         {
-            result = chop2Recursive(searchNum, 0, searchArray.length + 1,searchArray);
+            result = 0;
+        }
+        else if(searchArray.length != 0)
+        {
+            result = chop2Recursive(searchNum, 0, searchArray.length, searchArray);
         }
 
         return result;
@@ -98,9 +113,99 @@ public class KarateChop
     {
         int result = -1;
 
-        if(startIndex == 0 && (endIndex-1) == searchArray.length)
+        if(startIndex == 0 && endIndex == searchArray.length)
         {
+            int reducedEndIndex = endIndex / 2;
+            result = chop2CheckSide(searchNum, startIndex, reducedEndIndex, searchArray);
 
+            if(result == -1)
+            {
+                result = chop2Recursive(searchNum, startIndex, reducedEndIndex, searchArray);
+            }
+
+        }
+        else if(startIndex != endIndex)
+        {
+            result = chop2CheckSide(searchNum, startIndex, endIndex, searchArray);
+
+            if(result == -1)
+            {
+                int reducedStartIndex = endIndex;
+                int reducedEndIndex = searchArray.length - (endIndex / 2);
+
+                result = chop2Recursive(searchNum, reducedStartIndex, reducedEndIndex, searchArray);
+            }
+        }
+
+        return result;
+    }
+
+    private int chop2CheckSide(final int searchNum, final int startIndex, final int endIndex, final int[] searchArray)
+    {
+        int result = -1;
+        int reducedArrayLength = endIndex + 1;
+
+        if(endIndex >= searchArray.length)
+        {
+            reducedArrayLength = searchArray.length;
+        }
+
+
+        for(int i = startIndex; i < reducedArrayLength; i++)
+        {
+            if(searchArray[i] == searchNum)
+            {
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Pros: Concise. Quite verbose.
+     * Cons: Slow.
+     *
+     * I think this iteration could be made more concise without loosing verbosity.
+     * I like this iteration because of it's self documenting code.
+     * I feel like index calculation could be completely rethought, because I'm reusing same one from previous iterations
+     *  and I'm not really sure if it's the best.
+     *
+     * @param searchNum
+     * @param searchArray
+     * @return the integer index of the target in the array, or -1 if the target is not in the array.
+     */
+    public int chop3(final int searchNum, final int[] searchArray)
+    {
+        int result = -1;
+
+        if(searchArray.length > 0)
+        {
+            final List<Integer> integers = IntStream.of(searchArray).boxed().collect(Collectors.toList());
+
+            //Check first side
+            int middleIndex = (integers.size() - 1 ) /2;
+            result = integers.subList(0, middleIndex).indexOf(searchNum);
+
+            if(result == -1)
+            {
+                int startIndex = middleIndex;
+                middleIndex = integers.size() - (middleIndex / 2);
+
+                while(result == -1 && startIndex != middleIndex && startIndex < middleIndex)
+                {
+                    result = integers.subList(startIndex, middleIndex).indexOf(searchNum);
+
+                    if(result != -1)
+                    {
+                        result = result + startIndex;
+                    }
+
+                    startIndex = middleIndex;
+                    middleIndex = integers.size() - (middleIndex / 2);
+                }
+            }
         }
 
         return result;
@@ -161,5 +266,31 @@ public class KarateChop
         final long endTimeChop2 = System.currentTimeMillis() - startTimeChop2;
 
         System.out.println("Chop2 tests were completed in: " + endTimeChop2 + " ms");
+
+        final long startTimeChop3 = System.currentTimeMillis();
+        assertEquals(-1, obj.chop3(3, new int[]{}));
+        assertEquals(-1, obj.chop3(3, new int[]{1}));
+        assertEquals(0,  obj.chop3(1, new int[]{1}));
+
+        assertEquals(0,  obj.chop3(1, new int[]{1, 3, 5}));
+        assertEquals(1,  obj.chop3(3, new int[]{1, 3, 5}));
+        assertEquals(2,  obj.chop3(5, new int[]{1, 3, 5}));
+        assertEquals(-1, obj.chop3(0, new int[]{1, 3, 5}));
+        assertEquals(-1, obj.chop3(2, new int[]{1, 3, 5}));
+        assertEquals(-1, obj.chop3(4, new int[]{1, 3, 5}));
+        assertEquals(-1, obj.chop3(6, new int[]{1, 3, 5}));
+
+        assertEquals(0,  obj.chop3(1, new int[]{1, 3, 5, 7}));
+        assertEquals(1,  obj.chop3(3, new int[]{1, 3, 5, 7}));
+        assertEquals(2,  obj.chop3(5, new int[]{1, 3, 5, 7}));
+        assertEquals(3,  obj.chop3(7, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, obj.chop3(0, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, obj.chop3(2, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, obj.chop3(4, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, obj.chop3(6, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, obj.chop3(8, new int[]{1, 3, 5, 7}));
+        final long endTimeChop3 = System.currentTimeMillis() - startTimeChop3;
+
+        System.out.println("Chop3 tests were completed in: " + endTimeChop3 + " ms");
     }
 }
